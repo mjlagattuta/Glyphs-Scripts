50ab228f32df3af2db9f9a57c50b252b454609a1
Mike LaGattuta <mjlagattuta@gmail.com>
2018-10-26 21:07:09 -0400

    Updated genHOI to work with multiple paths

--- HOI/gen-hoi-layers.py
+++ HOI/gen-hoi-layers.py
@@ -8,30 +8,34 @@ currentGlyph = font.selectedLayers[0].parent.name
 		
 def addNode(layerId, idHOI, setting):
 	i = 0
-	for node in font.glyphs[currentGlyph].layers[layerId].paths[0].nodes:	
-		newNode = GSNode()
-		newNode.x = node.x
-		newNode.y = node.y
+	for path in font.glyphs[currentGlyph].layers[layerId].paths:
+		pathIndex = path.parent.indexOfPath_(path)
+		for node in path.nodes:
+			newNode = GSNode()
+			newNode.x = node.x
+			newNode.y = node.y
 		
-		if setting == 2:
-			newNode.type = "offcurve"
-		elif setting == 1:
-			newNode.type = "line"
-			font.glyphs[currentGlyph].layers[idHOI].paths.append(GSPath())
-		else:
-			newNode.type = "curve"
+			if setting == 2:
+				newNode.type = "offcurve"
+			elif setting == 1:
+				newNode.type = "line"
+				font.glyphs[currentGlyph].layers[idHOI].paths.append(GSPath())
+			else:
+				newNode.type = "curve"
+			print layerId, pathIndex, node.index, i
+			font.glyphs[currentGlyph].layers[idHOI].paths[i].nodes.append(newNode)
+			i = i + 1
 			
-		font.glyphs[currentGlyph].layers[idHOI].paths[i].nodes.append(newNode)
-		i = i + 1
 		
 
 def getFullName(layerName):
 	for layer in font.glyphs[currentGlyph].layers:
-		if re.match(layerName + ".*}$", layer.name):
+		if re.match(layerName + ".*}$", layer.name) != None:
 			return layer.name
+	return layerName
 	
 
-def populateHOILayer(aName, cName, dName, bName, hoiName):	
+def populateHOILayer(aName, cName, dName, bName, hoiName, cubicNum):	
 	cName = getFullName(cName)
 	dName = getFullName(dName)
 	bName = getFullName(bName)
@@ -42,13 +46,15 @@ def populateHOILayer(aName, cName, dName, bName, hoiName):
 		newHOILayer.paths = []
 		font.glyphs[currentGlyph].layers.append(newHOILayer)
 		
-	addNode(aName, hoiName, 1)
- 	addNode(cName, hoiName, 2)
- 	addNode(dName, hoiName, 2)
- 	addNode(bName, hoiName, 3)
+	if cubicNum == 1:
+		addNode(aName, hoiName, 1)
+	addNode(cName, hoiName, 2)
+	addNode(dName, hoiName, 2)
+	addNode(bName, hoiName, 3)
 
 
-populateHOILayer("Regular", "C1", "D1", "B", "B HOI")
-populateHOILayer("Regular Oblique", "E1", "F1", "G", "G HOI")
-populateHOILayer("Bold", "H1", "I1", "J", "J HOI")
-populateHOILayer("Bold Oblique", "K1", "L1", "M", "M HOI")
+populateHOILayer("Regular", "500 C1", "500 D1", "500 B", "B HOI", 1)
+populateHOILayer("Regular", "1000 C1", "1000 D1", "1000 B", "B HOI", 2)
+# populateHOILayer("Regular Oblique", "E1", "F1", "G", "G HOI")
+# populateHOILayer("Bold", "H1", "I1", "J", "J HOI")
+# populateHOILayer("Bold Oblique", "K1", "L1", "M", "M HOI")
